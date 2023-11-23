09c88468fc3e3d8a49b5f09c5530294ec761b1a6
Federico Ezequiel Massone <fedemassuno@gmail.com>
2023-11-23 19:24:04 -0300

    Clase 21

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from AppCoder.views import crear_curso, show_html, mostrar_cursos
+from AppCoder.views import crear_curso, show_html, mostrar_cursos, crear_curso_form,busqueda_camada
 
 urlpatterns = [
     path('agregar_curso/', crear_curso),
+    path('curso/', crear_curso_form),
     path('show/', show_html),
     path('cursos/', mostrar_cursos),
+    path('buscar/', busqueda_camada),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,30 +1,61 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from AppCoder.models import Curso
+from AppCoder.forms import CursoForm, BusquedaCursoForm
 
 
 def mostrar_cursos(request):
     cursos = Curso.objects.all()
     contexto = {
         "cursos": cursos,
-        "nombre": "Fede"
+        "nombre": "Fede",
+        "form": BusquedaCursoForm()
     }
     return render(request, "AppCoder/cursos.html", contexto)
 
 
 def crear_curso(request):
-    curso = Curso(nombre ="Python", camada=41000)
+    curso = Curso(nombre="Python", camada=41000)
     curso.save()
 
     contexto = {"curso": curso}
 
-    return redirect("/app/cursos/") # get
-    #return render(request, 'index.html', contexto)
-    #return HttpResponse(f"Su curso es {curso.nombre} y la camada es {curso.camada}")
+    return redirect("/app/cursos/")  # get
+
+
+def crear_curso_form(request):
+    if request.method == "POST":
+        # Crear curso
+        curso_formulario = CursoForm(request.POST)
+        if curso_formulario.is_valid():
+            informacion = curso_formulario.cleaned_data
+
+            curso_crear = Curso(nombre=informacion["nombre"], camada=informacion["camada"])
+            curso_crear.save()
+            return redirect("/app/cursos/")
+
+    curso_formulario = CursoForm()
+    contexto = {
+        "form": curso_formulario
+    }
+    return render(request, "AppCoder/crear_curso.html", contexto)
+
+
+def busqueda_camada(request):
+    nombre = request.GET["nombre"]  # Es un diccionario
+    cursos = Curso.objects.filter(nombre__icontains=nombre)
+
+    contexto = {
+
+        "cursos": cursos,
+        "nombre": "Fede",
+        "form": BusquedaCursoForm()
+
+    }
+    return render(request, "AppCoder/cursos.html", contexto)
+
 
 def show_html(request):
     curso = Curso.objects.first()
     contexto = {"curso": curso, "nombre": "Fede"}
     return render(request, 'index.html', contexto)
-
-
